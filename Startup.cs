3663d484515fabdab5a56443eb77bfec31f32b7b
Tandoh <anthony.tandoh@tanacom.io>
2021-11-23 18:05:17 +0000

    commt Docker File

--- Startup.cs
+++ Startup.cs
@@ -15,6 +15,7 @@ namespace MyDDAPI
 {
     public class Startup
     {
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -31,18 +32,19 @@ namespace MyDDAPI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
+            // if (env.IsDevelopment())
+            // {
+            //     app.UseDeveloperExceptionPage();
+            // }
+            // else
+            // {
+            //     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            //     app.UseHsts();
+            // }
 
             app.UseHttpsRedirection();
             app.UseMvc();
         }
+        
     }
 }
